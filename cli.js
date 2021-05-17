99369e9d42909638414ab89d4e9230eeae033462
Qrac <qrac.jp@gmail.com>
2021-05-17 15:52:11 +0900

    Fix webpack dev server config

--- cli.js
+++ cli.js
@@ -26,7 +26,7 @@ const webpackCompiler = webpack(webpackConfig)
 const devServerOptions = Object.assign(
   {},
   {
-    contentBase: path.join(__dirname, "src"),
+    contentBase: path.join(path.resolve(), "src"),
   }
 )
 
