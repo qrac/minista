c59c0426a5312c4fde35f92da5e0348a3bb896cc
Qrac <qrac.jp@gmail.com>
2021-06-21 12:32:45 +0900

    Fix test

--- test/src/pages/index.js
+++ test/src/pages/index.js
@@ -9,6 +9,7 @@ const Home = () => {
     <AppLayout>
       <Helmet>
         <title>Home</title>
+        <link rel="icon" href="/favicon.png" />
       </Helmet>
       <Comment text="Comment Test" />
       <h1>Hello</h1>
