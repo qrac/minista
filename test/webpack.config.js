7e498a5a9bb72f38cd98f43026a5b88bce81ac01
Qrac <qrac.jp@gmail.com>
2021-12-06 23:01:26 +0900

    Remove test

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -1,18 +1,7 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-const CopyPlugin = require("copy-webpack-plugin")
-
 const webpackConfig = {
   devServer: {
     open: ["/index.html"],
   },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "assets/custom.css",
-    }),
-    new CopyPlugin({
-      patterns: [{ from: "./static", to: "./", noErrorOnMissing: true }],
-    }),
-  ],
 }
 
 module.exports = webpackConfig
