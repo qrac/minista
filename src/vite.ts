d1e1a0150826b84317415106100ef4a6b25adc18
Qrac <qrac.jp@gmail.com>
2022-02-27 12:25:26 +0900

    Add insert assets tag for vite dev server

--- src/vite.ts
+++ src/vite.ts
@@ -2,7 +2,7 @@ import fs from "fs-extra"
 import path from "path"
 import url from "url"
 import { searchForWorkspaceRoot } from "vite"
-import type { Plugin } from "vite"
+import type { Plugin, InlineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import mdx from "@mdx-js/rollup"
 
@@ -61,16 +61,62 @@ export function virtualHtml(): Plugin {
           import.meta.url
         )
         const ministaHtml = fs.readFileSync(ministaHtmlURL, "utf8")
+        const assetTagStr = getAssetsTagStr(
+          server.config.inlineConfig.build?.rollupOptions?.input
+        )
+        const ministaReplacedHtml = ministaHtml.replace(
+          "<!-- VIRTUAL_HTML_ASSETS_TAG -->",
+          assetTagStr
+        )
+        console.log(server.config.inlineConfig.build?.rollupOptions?.input)
 
         server.middlewares.use((req, res, next) => {
           if (req.url!.endsWith(".html")) {
             res.statusCode = 200
-            res.end(ministaHtml)
+            res.end(ministaReplacedHtml)
             return
           }
           next()
         })
       }
     },
+  }
+}
+
+function getAssetsTagStr(input: any) {
+  !input && ""
+  const tags = []
+
+  if (typeof input === "string") {
+    const tag = getAssetsTag(input)
+    tags.push(tag)
+  } else if (Array.isArray(input) && input.length > 0) {
+    input.map((item) => {
+      const tag = getAssetsTag(item)
+      return tags.push(tag)
+    })
+  } else if (typeof input === "object") {
+    Object.values(input).map((item) => {
+      const tag = typeof item === "string" ? getAssetsTag(item) : ""
+      return tags.push(tag)
+    })
+  }
+
+  const sortedTags = tags.sort((a, b) => (a < b ? -1 : 1))
+  return sortedTags.join("\n")
+}
+
+function getAssetsTag(input: string) {
+  !input && ""
+
+  if (input.match(/\.(css|sass|scss)$/)) {
+    return `<link rel="stylesheet" href="${input}">`
+  } else if (input.match(/\.(js|cjs|mjs|jsx|ts|tsx)$/)) {
+    return `<script defer src="${input}"></script>`
+  } else {
+    console.log(
+      "Could not insert the entry [vite.build.rollupOptions.input] into the dev server."
+    )
+    return ""
   }
 }
