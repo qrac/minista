bbc0dbf2e9271373a8e4b0e799d79942edeaa512
Qrac <qrac.jp@gmail.com>
2022-03-02 21:23:40 +0900

    Fix optimizeCommentOutStyleImport

--- src/optimize.ts
+++ src/optimize.ts
@@ -3,17 +3,10 @@ import fs from "fs-extra"
 export async function optimizeCommentOutStyleImport(entryPoints: string[]) {
   await Promise.all(
     entryPoints.map(async (entryPoint) => {
-      fs.readFile(entryPoint, "utf8", function (err, data) {
-        if (err) {
-          return console.log(err)
-        }
-        const reg = new RegExp('(import ".*(css|sass|scss)";)', "g")
-        const result = data.replace(reg, "//$1")
-
-        fs.outputFile(entryPoint, result, "utf8", function (err) {
-          if (err) return console.log(err)
-        })
-      })
+      const data = await fs.readFile(entryPoint, "utf-8")
+      const reg = new RegExp('(import ".*(css|sass|scss)";)', "g")
+      const result = data.replace(reg, "//$1")
+      return fs.outputFile(entryPoint, result, "utf8")
     })
   )
 }
