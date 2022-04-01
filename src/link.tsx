8356414786ba7cb32ef3e81dacea3dc3dec15dad
Qrac <qrac.jp@gmail.com>
2022-04-01 18:29:17 +0900

    Fix link components

--- src/link.tsx
+++ src/link.tsx
@@ -3,13 +3,13 @@ import type { LinkProps as ReactRouterDomLinkProps } from "react-router-dom"
 import { Link as ReactRouterDomLink } from "react-router-dom"
 
 type LinkProps = ReactRouterDomLinkProps &
-  React.RefAttributes<HTMLAnchorElement> & { useDevOnlySpaLink?: boolean }
+  React.RefAttributes<HTMLAnchorElement>
 
 // Deprecated components
 export const Link = (props: LinkProps) => {
-  const { useDevOnlySpaLink = false, to, children, ...attribute } = props
+  const { to, children, ...attribute } = props
   const href = to.toString()
-  if (useDevOnlySpaLink) {
+  if (process.env.NODE_ENV === "development") {
     return (
       <ReactRouterDomLink to={to} {...attribute}>
         {children}
