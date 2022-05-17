a66eeeb037b8b470ee16c6d773079c890f3fed98
Qrac <qrac.jp@gmail.com>
2022-05-17 13:15:32 +0900

    Add declare #43

--- client.d.ts
+++ client.d.ts
@@ -1,3 +1,65 @@
+// CSS modules
+type CSSModuleClasses = { readonly [key: string]: string }
+
+declare module "*.module.css" {
+  const classes: CSSModuleClasses
+  export default classes
+}
+declare module "*.module.scss" {
+  const classes: CSSModuleClasses
+  export default classes
+}
+declare module "*.module.sass" {
+  const classes: CSSModuleClasses
+  export default classes
+}
+declare module "*.module.less" {
+  const classes: CSSModuleClasses
+  export default classes
+}
+declare module "*.module.styl" {
+  const classes: CSSModuleClasses
+  export default classes
+}
+declare module "*.module.stylus" {
+  const classes: CSSModuleClasses
+  export default classes
+}
+declare module "*.module.pcss" {
+  const classes: CSSModuleClasses
+  export default classes
+}
+
+// CSS
+declare module "*.css" {
+  const css: string
+  export default css
+}
+declare module "*.scss" {
+  const css: string
+  export default css
+}
+declare module "*.sass" {
+  const css: string
+  export default css
+}
+declare module "*.less" {
+  const css: string
+  export default css
+}
+declare module "*.styl" {
+  const css: string
+  export default css
+}
+declare module "*.stylus" {
+  const css: string
+  export default css
+}
+declare module "*.pcss" {
+  const css: string
+  export default css
+}
+
 declare module "*.mdx" {
   import { MDXProps } from "mdx/types"
   export default function MDXContent(props: MDXProps): JSX.Element
