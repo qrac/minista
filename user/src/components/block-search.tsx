c49cf138a9c1bf977e736bf2258bc25c10aa1f4b
Qrac <qrac.jp@gmail.com>
2022-06-12 12:45:16 +0900

    Enhanced search component #49

--- user/src/components/block-search.tsx
+++ user/src/components/block-search.tsx
@@ -1,21 +1,30 @@
 import { Search } from "../../../"
 
-//import "./block-search.css"
+import "./block-search.css"
 import SvgrSearch from "../assets/svgs/search.svg"
 
 const BlockSearch = () => {
   return (
-    <>
+    <div
+      style={{
+        maxWidth: "600px",
+        padding: "16px",
+        backgroundColor: "#f5f6f7",
+        borderRadius: "12px",
+      }}
+    >
       <Search
         jsonPath="/assets/search.json"
         //maxHitPages={2}
         className="block-search"
         placeholder="Search..."
-        searchFieldInsertBeforeElement={<SvgrSearch className="search-icon" />}
+        searchFieldInsertBeforeElement={
+          <SvgrSearch className="block-search-icon" />
+        }
         searchFieldClassName="block-search-field"
         searchListClassName="block-search-list"
       />
-    </>
+    </div>
   )
 }
 
