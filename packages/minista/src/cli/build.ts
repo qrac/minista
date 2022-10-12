0b497a0f043165805336d58f4c4a740df146b76f
Qrac <qrac.jp@gmail.com>
2022-10-12 11:50:56 +0900

    Move type #91 (#73)

--- packages/minista/src/cli/build.ts
+++ packages/minista/src/cli/build.ts
@@ -1,17 +1,7 @@
-import type { RollupOutput } from "rollup"
-
 import type { InlineConfig } from "../config/index.js"
 import { resolveConfig } from "../config/index.js"
 import { generateBundle } from "../generate/bundle.js"
 import { generateMain } from "../generate/main.js"
-
-export type BuildResult = {
-  output: BuildItem[]
-}
-type BuildItem = RollupOutput["output"][0] & {
-  source?: string
-  code?: string
-}
 
 export async function build(inlineConfig: InlineConfig = {}) {
   const config = await resolveConfig(inlineConfig)
