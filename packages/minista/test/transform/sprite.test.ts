a93d2bb8bf5928f40a3a3006a06bcdd42355a68e
Qrac <qrac.jp@gmail.com>
2022-11-08 13:31:51 +0900

    Fix test

--- packages/minista/test/transform/sprite.test.ts
+++ packages/minista/test/transform/sprite.test.ts
@@ -8,7 +8,7 @@ import { transformSprite } from "../../src/transform/sprite"
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-describe("compileSvgSprite", () => {
+describe("transformSprite", () => {
   it("Default", async () => {
     const svgFile = path.join(__dirname, "../_data/plus.svg")
     const options = {
