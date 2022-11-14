d5f2ccfca64a425ac7fe890b45c44255a19b2bf8
Qrac <qrac.jp@gmail.com>
2022-11-14 17:29:11 +0900

    Support for changing charset and viewport by Head component #103 (#79)

--- packages/minista/src/transform/page.tsx
+++ packages/minista/src/transform/page.tsx
@@ -93,41 +93,61 @@ export function transformPage({
       </HelmetProvider>
     </StaticRouter>
   )
-
   const { helmet } = helmetContext as FilledContext
 
-  const staticHelmet = (data: string) => {
-    return data.replace(/data-rh="true"/g, "")
+  let htmlAttributes = ` lang="ja" ${helmet.htmlAttributes.toString()}`
+  let bodyAttributes = ""
+
+  if (Object.hasOwn(helmet.htmlAttributes.toComponent(), "lang")) {
+    htmlAttributes = " " + helmet.htmlAttributes.toString()
+  }
+  if (helmet.bodyAttributes) {
+    bodyAttributes = " " + helmet.bodyAttributes.toString()
   }
 
-  const htmlAttributes = Object.hasOwn(
-    helmet.htmlAttributes.toComponent(),
-    "lang"
-  )
-    ? helmet.htmlAttributes.toString()
-    : `lang="ja" ${helmet.htmlAttributes.toString()}`
+  const metaAll = helmet.meta.toString()
+
+  const metaCharsetDefault = `<meta charset="UTF-8" />`
+  const metaCharsetReg = /<meta[^<>]*?charset=.*?\/>/gi
+  const metaCharsetArray = metaAll.match(metaCharsetReg) || [""]
+
+  const metaViewportDefault = `<meta name="viewport" content="width=device-width, initial-scale=1.0" />`
+  const metaViewportReg = /<meta[^<>]*?name="viewport".*?\/>/gi
+  const metaViewportArray = metaAll.match(metaViewportReg) || [""]
+
+  let metaCharset: string
+  metaCharset = metaCharsetArray.at(-1) || ""
+  metaCharset = metaCharset || metaCharsetDefault
+
+  let metaViewport: string
+  metaViewport = metaViewportArray.at(-1) || ""
+  metaViewport = metaViewport || metaViewportDefault
+
+  let metaOther: string
+  metaOther = metaAll || ""
+  metaOther = metaOther.replace(metaCharsetReg, "").replace(metaViewportReg, "")
 
   const insertHeadTags = headTags ? headTags : ""
   const insertStartTags = startTags ? `${startTags}\n` : ""
   const insertEndTags = endTags ? `\n${endTags}` : ""
 
-  const html = `
-  <!doctype html>
-  <html ${htmlAttributes}>
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      ${staticHelmet(helmet.title.toString())}
-      ${staticHelmet(helmet.meta.toString())}
-      ${staticHelmet(helmet.link.toString())}
-      ${staticHelmet(helmet.script.toString())}
-      ${insertHeadTags}
-      ${staticHelmet(helmet.style.toString())}
-    </head>
-    <body ${helmet.bodyAttributes.toString()}>
-      ${insertStartTags}${markup}${insertEndTags}
-    </body>
-  </html>
-`
-  return html
+  function staticHelmet(data: string) {
+    return data.replace(/\sdata-rh="true"/g, "")
+  }
+  return `<!doctype html>
+<html${htmlAttributes}>
+  <head>
+    ${staticHelmet(metaCharset)}
+    ${staticHelmet(metaViewport)}
+    ${staticHelmet(helmet.title.toString())}
+    ${staticHelmet(metaOther)}
+    ${staticHelmet(helmet.link.toString())}
+    ${staticHelmet(helmet.script.toString())}
+    ${insertHeadTags}
+    ${staticHelmet(helmet.style.toString())}
+  </head>
+  <body${bodyAttributes}>
+    ${insertStartTags}${markup}${insertEndTags}
+  </body>
+</html>`
 }
--- playground/shared-head/src/pages/index.tsx
+++ playground/shared-head/src/pages/index.tsx
@@ -7,10 +7,13 @@ export default function () {
         htmlAttributes={{ lang: "en" }}
         bodyAttributes={{ class: "custom-body" }}
       >
+        <meta charSet="shift_jis" />
+        <meta name="viewport" content="width=500, initial-scale=1" />
         <title>HEAD: CUSTOM TITLE</title>
         <meta name="description" content="description" />
       </Head>
       <h1>index</h1>
+      <p>日本語の文字</p>
     </>
   )
 }
