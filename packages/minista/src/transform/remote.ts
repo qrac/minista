fd12b1f2b061888558e4bb2069e184d6b774d1f1
Qrac <qrac.jp@gmail.com>
2023-01-16 18:19:40 +0900

    Optimize remote image #98 (#76)

--- packages/minista/src/transform/remote.ts
+++ packages/minista/src/transform/remote.ts
@@ -10,30 +10,10 @@ import type { ParsedPage } from "../cli/build.js"
 import type { TempRemotes } from "../generate/remote.js"
 import { generateTempRemote } from "../generate/remote.js"
 
-export function getRemoteFileName(url: string) {
+export function getRemoteExt(url: string) {
   const pathname = parseUrl(url).pathname || ""
   const parsedName = path.parse(pathname)
-  const fileName = parsedName.base
-  const hasExt = parsedName.ext
-
-  return hasExt ? fileName : ""
-}
-
-export function getRemoteFileExt(url: string) {
-  const pathname = parseUrl(url).pathname || ""
-  const parsedName = path.parse(pathname)
-
   return parsedName.ext.replace(/^\./, "") || ""
-}
-
-export function splitRemoteUrls(remoteUrls: string[]) {
-  const hasNameUrls = remoteUrls.filter((url) => {
-    return getRemoteFileName(url)
-  })
-  const noNameUrls = remoteUrls.filter((url) => {
-    return !hasNameUrls.includes(url)
-  })
-  return { hasNameUrls, noNameUrls }
 }
 
 async function fetchRemote({
@@ -44,8 +24,8 @@ async function fetchRemote({
 }: {
   url: string
   outDir: string
-  remoteName?: string
-  remoteCount?: number
+  remoteName: string
+  remoteCount: number
 }) {
   let fileName = ""
   let extName = ""
@@ -58,17 +38,11 @@ async function fetchRemote({
     return { url, fileName, data: "" }
   }
   contentType = res.headers.get("content-type") || ""
-  extName = extension(contentType) || getRemoteFileExt(url)
+
+  extName = extension(contentType) || getRemoteExt(url)
   extName = extName === "jpeg" ? "jpg" : extName
 
-  if (remoteName && remoteCount) {
-    fileName = `${remoteName}-${remoteCount}.${extName}`
-  } else {
-    fileName = getRemoteFileName(url)
-  }
-  if (!fileName) {
-    return { url, fileName, data: "" }
-  }
+  fileName = `${remoteName}-${remoteCount}.${extName}`
   fileName = path.join(outDir, fileName)
 
   const arrayBuffer = await res.arrayBuffer()
@@ -77,22 +51,19 @@ async function fetchRemote({
   return { url, fileName, data: buffer }
 }
 
-export async function transformRemotes({
-  parsedHtml,
+export async function transformRemote({
+  remoteEls,
   config,
   tempRemotes,
 }: {
-  parsedHtml: NHTMLElement
+  remoteEls: NHTMLElement[]
   config: ResolvedConfig
   tempRemotes: TempRemotes
 }) {
-  const { tempDir } = config.sub
+  const { resolvedRoot, tempDir } = config.sub
   const { remoteName } = config.main.assets.images
   const outDir = path.join(tempDir, "images", "remote")
 
-  const remoteEls = parsedHtml.querySelectorAll(
-    `[data-minista-transform-target="remote"]`
-  )
   const remoteSrcs = remoteEls.map((el) => {
     el.setAttribute("data-minista-transform-target", "image")
     return el.getAttribute("data-minista-image-src") || ""
@@ -123,6 +94,29 @@ export async function transformRemotes({
       })
     })
   )
+  return remoteEls.map((el) => {
+    const remoteSrc = el.getAttribute("data-minista-image-src") || ""
+    const fileName = tempRemotes[remoteSrc].fileName || ""
+    const filePath = fileName.replace(resolvedRoot, "")
+    el.setAttribute("data-minista-image-src", filePath)
+    el.setAttribute("data-minista-transform-target", "image")
+    return
+  })
+}
+
+export async function transformRemotes({
+  parsedHtml,
+  config,
+  tempRemotes,
+}: {
+  parsedHtml: NHTMLElement
+  config: ResolvedConfig
+  tempRemotes: TempRemotes
+}) {
+  const remoteEls = parsedHtml.querySelectorAll(
+    `[data-minista-transform-target="remote"]`
+  )
+  await transformRemote({ remoteEls, config, tempRemotes })
   return parsedHtml
 }
 
@@ -135,52 +129,12 @@ export async function transformRemotesAll({
   config: ResolvedConfig
   tempRemotes: TempRemotes
 }) {
-  const { tempDir } = config.sub
-  const { remoteName } = config.main.assets.images
-  const outDir = path.join(tempDir, "images", "remote")
-
-  const remoteSrcsAll = parsedPages.map((page) => {
-    const remoteEls = page.parsedHtml.querySelectorAll(
+  const remoteElsAll = parsedPages.map((page) => {
+    return page.parsedHtml.querySelectorAll(
       `[data-minista-transform-target="remote"]`
     )
-    const remoteSrcs = remoteEls.map((el) => {
-      el.setAttribute("data-minista-transform-target", "image")
-      return el.getAttribute("data-minista-image-src") || ""
-    })
-    return remoteSrcs
   })
-  const remoteUrls = [...new Set(remoteSrcsAll.flat())].sort()
-  const { hasNameUrls, noNameUrls } = splitRemoteUrls(remoteUrls)
-
-  const hasNameRemotes = await Promise.all(
-    hasNameUrls.map(async (url) => {
-      return await fetchRemote({
-        url,
-        outDir,
-      })
-    })
-  )
-  const noNameRemotes = await Promise.all(
-    noNameUrls.map(async (url, index) => {
-      return await fetchRemote({
-        url,
-        outDir,
-        remoteName,
-        remoteCount: index + 1,
-      })
-    })
-  )
-  const createRemotes = [...hasNameRemotes, ...noNameRemotes]
-
-  await Promise.all(
-    createRemotes.map(async (item) => {
-      return await generateTempRemote({
-        url: item.url,
-        fileName: item.fileName,
-        data: item.data,
-        tempRemotes,
-      })
-    })
-  )
+  const remoteEls = remoteElsAll.flat()
+  await transformRemote({ remoteEls, config, tempRemotes })
   return parsedPages
 }
