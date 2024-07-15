ce580cdee120a14c21d74da7408202c7415a638e
Qrac <qrac.jp@gmail.com>
2024-07-15 19:57:41 +0900

    Integrating plugin entry into the plugin bundle

--- playground/bundle/src/pages/index.tsx
+++ playground/bundle/src/pages/index.tsx
@@ -1,9 +1,16 @@
+import { Head } from "minista-plugin-ssg/client"
+
 import { Button } from "../components/button"
 import iconUrl from "../assets/icon.png"
 
 export default function () {
   return (
     <>
+      <Head>
+        <link rel="stylesheet" href="/src/assets/style.css" data-test="" />
+        <link rel="stylesheet" href="/src/assets/dummy.css" />
+        <script type="module" src="/src/assets/script.ts" />
+      </Head>
       <h1>Index</h1>
       <ul>
         <li>
--- playground/entry/minista.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "minista"
-import { pluginEnhance } from "minista-plugin-enhance"
-import { pluginEntry } from "minista-plugin-entry"
-
-export default defineConfig({
-  plugins: [pluginEnhance(), pluginEntry()],
-})
