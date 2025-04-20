17fd03f8f935b281bebf9792103a1e9d92725f55
Qrac <qrac.jp@gmail.com>
2025-04-20 14:22:53 +0900

    Fix type definitions in package.json and enhance client.d.ts exports

--- packages/minista/types/client.d.ts
+++ packages/minista/types/client.d.ts
@@ -1,5 +1,13 @@
 /// <reference types="vite/client" />
 
+export type {
+  Metadata,
+  PageProps,
+  LayoutProps,
+  GetStaticData,
+  StaticData,
+} from "../src/plugin-ssg/types"
+
 import type { HeadData } from "../src/head/types"
 
 export declare function Head(
