08d7214cdadaed2453797d5a36fa8a414ce21f75
Qrac <qrac.jp@gmail.com>
2025-04-20 13:54:34 +0900

    Add public playground with configuration and update index pages

---
+++ playground/public/src/pages/nest/index.tsx
@@ -0,0 +1,20 @@
+import { Head } from "minista/client"
+
+export default function () {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/image.png" />
+      </Head>
+      <h1>Nest</h1>
+      <ul>
+        <li>
+          <a href="/nest/">Nest</a>
+        </li>
+      </ul>
+      <div>
+        <img src="/image2.png" alt="icon" width={76} height={76} />
+      </div>
+    </>
+  )
+}
