b7b2310d889edb65b692c7c6b950dc79d8eebc80
Qrac <qrac.jp@gmail.com>
2026-01-09 23:55:00 +0900

    Fix regImage

--- packages/minista/src/shared/reg.js
+++ packages/minista/src/shared/reg.js
@@ -1,3 +1,3 @@
 export const regScript = /\.(?:js|mjs|cjs|jsx|ts|tsx|mts|cts)$/i
 export const regStyle = /\.(?:css|scss|sass|less|styl)$/i
-export const regImage = /\.(?:png|jpg|jpeg|gif|bmp|svg|webp)$/i
+export const regImage = /\.(?:png|jpe?g|gif|bmp|svg|webp|avif)$/i
